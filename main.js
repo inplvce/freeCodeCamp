3b9dea0eb9ab86d2400ef809bda8194fde9c9297
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-09 22:05:48 +0300

    new

--- main.js
+++ main.js
@@ -2,3 +2,22 @@
 
 const a = 9;
 console.log(a);
+
+const myStr = "FirstLine" + "\n\t\\SecondLine" + "\nThirdLine";
+console.log(myStr);
+
+// Создайте вложенный массив с именем myArray.
+
+const myArray = [["Joy", 19], [18, "Jean"]];
+
+// Create a variable called myData and set it to equal the first value of myAarray using bracket notation.
+
+const myAarray = [50, 60, 70];
+
+let myData = myAarray[0];
+
+// Измените данные, хранящиеся в индексе 0, myArrayна значение 45.
+
+const mYArray = [18, 64, 99];
+
+myArray[0] = 45;
