e9d64baadf3624aef23bf78de205edb498f28081
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-11 23:46:25 +0300

    n

--- main.js
+++ main.js
@@ -200,3 +200,17 @@ return "10 or Under";
 }
 testGreaterThan(10);
 
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+}
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+}
+
+return "Less than 10";
+}
+
+testGreaterOrEqual(10);
+
