c02e72b238bb8e686b9a9e091f9403eabb63cee5
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-11 23:48:21 +0300

    n

--- main.js
+++ main.js
@@ -214,3 +214,17 @@ return "Less than 10";
 
 testGreaterOrEqual(10);
 
+
+function testLessOrEqual(val) {
+    if (val <= 12) {  // Change this line
+        return "Smaller Than or Equal to 12";
+    }
+
+    if (val <= 24) {  // Change this line
+        return "Smaller Than or Equal to 24";
+    }
+
+    return "More Than 24";
+}
+
+testLessOrEqual(10);
