31dbfe1e8718e60fdaa5c312b84a51fc3303aa6c
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-11 23:37:49 +0300

    n

--- main.js
+++ main.js
@@ -187,4 +187,16 @@ testStrictNotEqual(10);
 
 // Добавьте оператор «больше чем» к указанным строкам, чтобы операторы return имели смысл.
 
+function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+    return "Over 100";
+}
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+}
+
+return "10 or Under";
+}
+testGreaterThan(10);
 
