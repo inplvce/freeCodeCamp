20329bfa2a2d13906b01c7c7d4e95d7696e15bf6
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-12 00:08:45 +0300

    n

--- main.js
+++ main.js
@@ -276,3 +276,17 @@ return result;
 }
 
 testElse(4);
+
+// Преобразуйте логику, чтобы использовать else ifоператоры.
+
+function testElseIf(val) {
+    if (val > 10) {
+        return "Greater than 10";
+    } else if (val < 5) {
+        return "Smaller than 5";
+    } else {
+
+    return "Between 5 and 10";
+}
+}
+testElseIf(7);
