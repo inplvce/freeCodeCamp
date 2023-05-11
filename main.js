cc8acb9541685489d348f090cf6cac5767ced54a
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-11 23:18:52 +0300

    n

--- main.js
+++ main.js
@@ -175,3 +175,12 @@ return "Equal";
 
 testNotEqual(10);
 
+
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Change this line
+    return "Not Equal";
+}
+return "Equal";
+}
+
+testStrictNotEqual(10);
