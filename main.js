eb52584525d79bd854d1bdf9f7a2d26596536a52
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-12 00:06:22 +0300

    n

--- main.js
+++ main.js
@@ -260,3 +260,19 @@ if (val < 10 || val > 20) {
 }
 
 testLogicalOr(15);
+
+// Объедините ifутверждения в одно if/elseутверждение.
+
+function testElse(val) {
+let result = "";
+// Only change code below this line
+if (val > 5) {
+result = "Bigger than 5";
+} else {
+result = "5 or Smaller";
+}
+// Only change code above this line
+return result;
+}
+
+testElse(4);
