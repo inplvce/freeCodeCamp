ef7cfb3127123d4e127652236341a385a36a7dff
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-05-13 00:39:46 +0300

    n

--- main.js
+++ main.js
@@ -317,19 +317,47 @@ num >= 20- вернутьсяHuge
 
 function testSize(num) {
     if (num < 5) {
-        return "Tiny";
-    } else if (num < 10) {
-        return "Small";
-    } else if (num < 15) {
-        return "Medium";
-    } else if (num < 20) {
-        return "Large";
-    } else if (num >= 20) {
-        return "Huge";
-    } else {
+            return "Tiny";
+        } else if (num < 10) {
+            return "Small";
+        } else if (num < 15) {
+            return "Medium";
+        } else if (num < 20) {
+            return "Large";
+        } else if (num >= 20) {
+            return "Huge";
+        }   else {
     
-        return "Change Me";
+            return "Change Me";
+        }
     }
-    }
-    
     testSize(7);
+
+
+    /*
+    par and strokes will always be numeric and positive. 
+    We have added an array of all the names for your convenience.
+    */
+
+    const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+    function golfScore(par, strokes) {
+      // Only change code below this line
+    if (strokes === 1) {
+        return names[0];
+    } else if (strokes <= par - 2) {
+        return names[1];
+    } else if (strokes === par - 1) {
+        return names[2];
+    } else if (strokes === par) {
+        return names[3];
+    } else if (strokes === par + 1) {
+        return names[4];
+    } else if (strokes === par + 2) {
+        return names[5];
+    } else {
+    return names[6];
+      // Only change code above this line
+    }
+    }
+    golfScore(5, 4);    
