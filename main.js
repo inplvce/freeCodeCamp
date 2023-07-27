b5dc7efe895f2d2c7c9f4cdb66b5c81e23e4f39f
Uladzimer Kaniushenka <ukaniushenka@gmail.com>
2023-07-28 00:37:53 +0300

    n

--- main.js
+++ main.js
@@ -367,3 +367,17 @@ function testSize(num) {
         return num * 5
         }
         timesFive(5)
+
+        /* Измените порядок логики в функции, 
+        чтобы она возвращала правильные операторы во всех случаях.*/
+    function orderMyLogic(val) {
+    if (val < 5) {
+        return "Less than 5";
+    } else if (val < 10) {
+        return "Less than 10";
+    } else {
+        return "Greater than or equal to 10";
+    }
+    }
+    
+    orderMyLogic(4);
